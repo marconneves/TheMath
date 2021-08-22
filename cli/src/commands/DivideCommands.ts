7b087c96e87e76ac5758953a8e6324ab23ec0611
Marcon Willian <marcon@mh4sh.dev>
2021-08-22 13:51:52 -0300

    chore ✨: Update divide and multiply

---
+++ cli/src/commands/DivideCommands.ts
@@ -0,0 +1,41 @@
+import TheMath from 'the-math';
+import chalk from 'chalk';
+
+import { Flags } from '../utils/parseArgumentsIntoOptions';
+
+const DivideCommands = {
+  async index(params: string[], flags: Flags): Promise<void> {
+    const numbers = params.map(numberString => {
+      if (flags.float) {
+        return Number(numberString);
+      }
+      return Math.round(Number(numberString));
+    });
+
+    const result = numbers.reduce(
+      (previousValue: number, currentValue: number, index, arrayOriginal) => {
+        if (flags.verbose && index > 0) {
+          const restParams = arrayOriginal
+            .slice(index + 1)
+            .map(numberOfArray => ` ÷ ${numberOfArray}`);
+
+          console.log(
+            restParams.length > 0 ? '= %s%s' : '= %s',
+            chalk.yellow.bold(`${previousValue} ÷ ${currentValue}`),
+            chalk.gray.bold(restParams.join(''))
+          );
+        }
+
+        if (!previousValue) {
+          return currentValue;
+        }
+
+        return TheMath.divide(previousValue, currentValue);
+      }
+    );
+
+    console.log('= %s', chalk.green.bold(result));
+  }
+};
+
+export default DivideCommands;
--- cli/src/commands/index.ts
+++ cli/src/commands/index.ts
@@ -3,6 +3,8 @@ import { Flags } from '../utils/parseArgumentsIntoOptions';
 
 import AddCommands from './AddCommands';
 import SubtractCommands from './SubtractCommands';
+import MultiplyCommands from './MultiplyCommands';
+import DivideCommands from './DivideCommands';
 
 export default async (program: CommanderCLI<Flags>): Promise<void> => {
   await program.command('add', AddCommands.index);
@@ -10,4 +12,8 @@ export default async (program: CommanderCLI<Flags>): Promise<void> => {
   await program.command('add', 'odd', AddCommands.odd);
 
   await program.command('subtract', SubtractCommands.index);
+
+  await program.command('multiply', MultiplyCommands.index);
+
+  await program.command('divide', DivideCommands.index);
 };
