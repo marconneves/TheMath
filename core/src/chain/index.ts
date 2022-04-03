a19282bc606ebb2f89f0c16942d98070a8df57d5
Marcon Willian <marconwillian@protonmail.com>
2022-04-03 02:10:22 -0300

    chore 🚧: Implement settings of chain

--- core/src/chain/index.ts
+++ core/src/chain/index.ts
@@ -2,33 +2,79 @@ import divide from '../operations/divide';
 import multiply from '../operations/multiply';
 import subtract from '../operations/subtract';
 import sum from '../operations/sum';
+import round from './round';
 
-interface IChain {
+export interface IRoundOptions {
+  /**
+   * @default 'round'
+   * @doc
+   * floor - rounds down
+   *
+   * ceil - rounds up
+   *
+   * round - rounds to nearest
+   */
+  type?: 'floor' | 'ceil' | 'round';
+  decimal?: number;
+}
+
+export interface IChainOptions {
+  round?: boolean | IRoundOptions;
+}
+
+export interface IChainOptionsGlobal {
+  round?: IRoundOptions;
+}
+
+export interface IChain {
   divide(divisor: number): IChain;
   multiply(multiplier: number): IChain;
   subtract(subtractor: number): IChain;
   add(adder: number): IChain;
-  done(): number;
+  done(options?: IChainOptions): number;
 }
 
-function chain(startNumber: number): IChain {
-  return {
-    divide(divisor: number): IChain {
-      return chain(divide(startNumber, divisor));
-    },
-    multiply(multiplier: number): IChain {
-      return chain(multiply(startNumber, multiplier));
-    },
-    subtract(subtractor: number): IChain {
-      return chain(subtract(startNumber, subtractor));
-    },
-    add(adder: number): IChain {
-      return chain(sum(startNumber, adder));
-    },
-    done(): number {
-      return startNumber;
-    }
+function createChain(globalInitOptions?: IChainOptions) {
+  const globalOptions = { ...globalInitOptions } as IChainOptionsGlobal;
+  if (typeof globalInitOptions?.round === 'boolean') {
+    globalOptions.round = { type: 'round' };
+  }
+
+  return function chain(startNumber: number): IChain {
+    return {
+      divide(divisor: number): IChain {
+        return chain(divide(startNumber, divisor));
+      },
+      multiply(multiplier: number): IChain {
+        return chain(multiply(startNumber, multiplier));
+      },
+      subtract(subtractor: number): IChain {
+        return chain(subtract(startNumber, subtractor));
+      },
+      add(adder: number): IChain {
+        return chain(sum(startNumber, adder));
+      },
+      done(options): number {
+        const operation = {
+          number: startNumber
+        };
+
+        if (globalOptions.round || options?.round) {
+          let roundOptions = {};
+
+          if (typeof options?.round !== 'boolean') {
+            roundOptions = {
+              roundType: options?.round?.type || globalOptions.round?.type,
+              decimal: options?.round?.decimal || globalOptions.round?.decimal
+            };
+          }
+
+          round(operation, roundOptions);
+        }
+
+        return operation.number;
+      }
+    };
   };
 }
-
-export default chain;
+export default createChain;
--- core/src/index.ts
+++ core/src/index.ts
@@ -4,7 +4,9 @@ import multiply from './operations/multiply';
 import sum from './operations/sum';
 
 import resolve from './resolve';
-import chain from './chain';
+import createChain from './chain';
+
+const chain = createChain();
 
 const TheMath = {
   subtract,
@@ -16,8 +18,10 @@ const TheMath = {
   multiply,
   m: multiply,
   resolve,
-  chain
+  chain,
+  createChain
 };
 
+export { IChainOptions, IChainOptionsGlobal, IRoundOptions } from './chain';
 export { resolve, chain };
 export default TheMath;
